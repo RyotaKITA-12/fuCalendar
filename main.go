4987d6671fd8894919365e8db07fc37ff69401cf
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-06-05 07:53:54 +0900

    remove import

--- main.go
+++ main.go
@@ -10,13 +10,6 @@ import (
 
 func main() {
     fmt.Println(models.Db)
-    u := &models.User{}
-    u.Name = "test"
-    u.Email = "test@example.com"
-    u.Password = "test"
-    fmt.Println(u)
-
-    u.CreateUser()
 
     controllers.StartMainServer()
 

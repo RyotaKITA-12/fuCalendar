c2058736b53ae46d4f3b776b879940c8d035ba0d
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-06-05 07:51:49 +0900

    test

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 
 func main() {
     fmt.Println(models.Db)
+    u := &models.User{}
+    u.Name = "test"
+    u.Email = "test@example.com"
+    u.Password = "test"
+    fmt.Println(u)
+
+    u.CreateUser()
+
     controllers.StartMainServer()
 
 }

e699eda2c6c7559b2dca509ad2cfe0c18e7b606b
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-06-05 10:19:52 +0900

    お願い成功して

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -14,7 +14,8 @@ func index(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         http.Redirect(w, r, "/", 302)
     } else {
-        user, err := sess.GetUserBySession()
+        _, err := sess.GetUserBySession()
+        // user, err := sess.GetUserBySession()
         if err != nil {
             log.Println(err)
         }

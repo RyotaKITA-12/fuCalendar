3ccef940e44465c653ef9ece6366808866b488f3
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-06-05 09:54:18 +0900

    test

--- main.go
+++ main.go
@@ -4,26 +4,20 @@ import (
 	"fmt"
 	"time"
 
-	// "github.com/RyotaKITA-12/fuCalendar.git/app/controllers"
+	"github.com/RyotaKITA-12/fuCalendar.git/app/controllers"
 	"github.com/RyotaKITA-12/fuCalendar.git/app/models"
 	// "log"
 )
 
 func main() {
 	fmt.Println(models.Db)
-
-	// controllers.StartMainServer()
-
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Password = "testtest"
-	fmt.Println(u)
-
-	u.CreateUser()
-
+    u := &models.User{}
+    u.Name = "test_001"
+    u.Email = "test_001@example.com"
+    u.Password = "test_001"
+    fmt.Println(u)
+    u.CreateUser()
     u.CreateEvent("test-content", "渋谷", time.Now(), time.Now().AddDate(0, 0, 1), 1)
+	controllers.StartMainServer()
+    // e, _ := u.GetEventsByUser()
 
-    e, _ := u.GetEventsByUser()
-    fmt.Println(e)
-}

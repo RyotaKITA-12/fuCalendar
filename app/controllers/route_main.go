1431ab67038793d271627d51532f5b578bd9b798
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-06-07 22:57:49 +0900

    test

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -14,15 +14,15 @@ func index(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         http.Redirect(w, r, "/", 302)
     } else {
-        user, err := sess.GetUserBySession()
+        _, err := sess.GetUserBySession()
         if err != nil {
             log.Println(err)
         }
-        events, err := user.GetEventsByUser()
-        if err != nil {
-            log.Println(err)
-        }
-        user.Events = events
+        // events, err := user.GetEventsByUser()
+        // if err != nil {
+        //     log.Println(err)
+        // }
+        // user.Events = events
         generateHTML(w, nil, "layout", "private_navbar", "index")
     }
 }

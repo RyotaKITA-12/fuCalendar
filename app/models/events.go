f1486ccb88091f400c98470589fd36de6e6cb05d
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-06-08 18:51:02 +0900

    test

--- app/models/events.go
+++ app/models/events.go
@@ -6,22 +6,22 @@ import (
 )
 
 type Event struct {
-	ID         int
-	Content    string
-	Location   string
-    StartTime time.Time
-    EndTime   time.Time
-    HostID     int
-    GroupID     int
+	ID        int
+	Content   string
+	Location  string
+	StartTime time.Time
+	EndTime   time.Time
+	HostID    int
+	GroupID   int
 	CreatedAt time.Time
 }
 
 func (u *User) CreateEvent(content string,
-                           location string,
-                           start_time time.Time,
-                           end_time time.Time,
-                           g_id int) (err error) {
-    cmd := `INSERT INTO events (
+	location string,
+	start_time time.Time,
+	end_time time.Time,
+	g_id int) (err error) {
+	cmd := `INSERT INTO events (
                 content,
                 location,
                 start_time,
@@ -30,12 +30,12 @@ func (u *User) CreateEvent(content string,
                 g_id,
                 created_at)
             VALUES ($1, $2, $3, $4, $5, $6, &7)`
-    _, err = Db.Exec(cmd, content, location, start_time, end_time,
-                     u.ID, g_id, time.Now())
-    if err != nil {
-        log.Fatalln(err)
-    }
-    return err
+	_, err = Db.Exec(cmd, content, location, start_time, end_time,
+		u.ID, g_id, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
 }
 
 // func GetEvent(id int) (event Event, err error) {
@@ -85,34 +85,35 @@ func (u *User) CreateEvent(content string,
 // }
 
 func (u *User) GetEventsByUser() (events []Event, err error) {
-    cmd := `SELECT id, content, location, start_time, end_time,
+	cmd := `SELECT id, content, location, start_time, end_time,
                    host_id, g_id, created_at
             FROM events
             WHERE host_id = $1`
-    rows, err := Db.Query(cmd, u.ID)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    for rows.Next() {
-        var event Event
-        err = rows.Scan(
-            &event.ID,
-            &event.Content,
-            &event.Location,
-            &event.StartTime,
-            &event.EndTime,
-            &event.HostID,
-            &event.GroupID,
-            &event.CreatedAt)
-        if err != nil {
-            log.Fatalln(err)
-        }
-        events = append(events, event)
-    }
-    rows.Close()
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var event Event
+		err = rows.Scan(
+			&event.ID,
+			&event.Content,
+			&event.Location,
+			&event.StartTime,
+			&event.EndTime,
+			&event.HostID,
+			&event.GroupID,
+			&event.CreatedAt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		events = append(events, event)
+	}
+	rows.Close()
 
-    return events, err
+	return events, err
 }
+
 // func (e *Event) UpdateEvent() error {
 //     cmd := `UPDATE events SET content = $1, location = $2, user_id = $3,
 //                    start_time = $4, end_time = $5

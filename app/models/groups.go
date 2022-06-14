40581b6f6e8edad503445e94545a9f2136a9fb53
RyotaKITA-12 <s202207@stu.musashino-u.ac.jp>
2022-06-14 14:31:43 +0900

    group

--- app/models/groups.go
+++ app/models/groups.go
@@ -29,18 +29,18 @@ func (u *User) CreateGroup(name string, gests []User) (err error) {
 	return err
 }
 
-func (u *User) GetGroupByName(name string) (group Group, err error) {
-    group = Group{}
-    cmd := `SELECT id, name, host_id, gest_id, created_at
-            FROM groups
-            WHERE name = $1 AND host_id = $2`
-    err = Db.QueryRow(cmd, name, u.ID).Scan(
-        &group.ID,
-        &group.Name,
-        &group.ID,
-        &group.ID,
-        &group.ID,
-        &group.ID,
-    )
-
-}
+// func (u *User) GetGroupByName(name string) (group Group, err error) {
+//     group = Group{}
+//     cmd := `SELECT id, name, host_id, gest_id, created_at
+//             FROM groups
+//             WHERE name = $1 AND host_id = $2`
+//     err = Db.QueryRow(cmd, name, u.ID).Scan(
+//         &group.ID,
+//         &group.Name,
+//         &group.ID,
+//         &group.ID,
+//         &group.ID,
+//         &group.ID,
+//     )
+//
+// }
